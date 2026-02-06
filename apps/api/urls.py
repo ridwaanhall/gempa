bdb8ec7b061896b1e96a8299371e4b6c54cd1908
Ridwan Halim <hi@ridwaanhall.com>
2026-02-07 02:05:37 +0700

    🔧fix(api): Organize URL patterns for better readability and maintainability

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -9,10 +9,16 @@ urlpatterns = [
 	path("tsunami/", views.TsunamiAlertView.as_view(), name="earthquake-tsunami"),
 	path("felt/", views.FeltAlertView.as_view(), name="earthquake-felt"),
 	path("m5/", views.M5AlertView.as_view(), name="earthquake-m5"),
+ 
+    # historical
 	path("mon3/", views.Mon3View.as_view(), name="earthquake-mon3"),
 	path("yr5/", views.Yr5View.as_view(), name="earthquake-yr5"),
+ 
+    # seismic
 	path("seismic-indo/", views.SeismicView.as_view(), name="seismic-indo"),
 	path("seismic-global/", views.GlobalSensorView.as_view(), name="seismic-global"),
+ 
+    # faults
 	path("faults-indo/", views.FaultsIndoView.as_view(), name="faults-indo"),
 	path("faults-global/", views.FaultsGlobalView.as_view(), name="faults-global"),
 ]
